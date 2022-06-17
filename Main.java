8b03d75dde2a65d9cb0789ebd9bfe706c8f1ef27
314dhan <90907951+314dhan@users.noreply.github.com>
2022-06-17 23:40:11 +0700

    menambahkan main program

---
+++ Main.java
@@ -0,0 +1,36 @@
+package Latihan.rumahsakit;
+
+import java.util.Scanner;
+import Latihan.rumahsakit.dokter.Dokter;
+
+public class Main{
+    public static void main(String[] args) {
+        int pilihan;
+        char choice;
+        Scanner input = new Scanner(System.in);
+        Dokter dok = new Dokter();
+        
+        System.out.println("pilih no Rawat : ");
+        System.out.println("1. Rawat Inap");
+        System.out.println("2. Rawat Jalan");
+        System.out.print("Pilih [1 / 2]: ");
+        
+        pilihan = input.nextInt();
+        if(pilihan == 1){
+            dok.rawatInap();
+        }else if(pilihan == 2){
+            dok.rawatJalan();
+        }else{
+            System.out.println("pilihan tidak tersedia");
+        }
+
+        System.out.print("\nApakah anda ingin mengulangi program? [y/n]: ");
+        choice = input.next().charAt(0);
+        if(choice == 'y'){
+            main(args);
+        }else{
+            System.out.println("Terima Kasih telah menggunakan jasa kami");    
+        }
+        input.close();
+    }  
+}
